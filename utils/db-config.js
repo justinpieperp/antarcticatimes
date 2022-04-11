e0acf46b6fbd0e04d195c9e6e9bdfda335a1eb54
ruoxuanw <penguinineteen@gmail.com>
2022-04-10 18:57:18 -0700

    staged

--- utils/db-config.js
+++ utils/db-config.js
@@ -6,7 +6,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wwmse.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    console.log('Successfully Connected to MongoDB: 7-myblog')
+    console.log('Successfully Connected to MongoDB')
   })
   .catch((err) => {
     console.log('Error', err)
